411011bdb0a1faa2472401f3fc22fc957b26ef4a
Nox <nox.tux@gmail.com>
2023-04-29 13:01:58 -0400

    MoveComponent now moves through the rigidbody

--- LudumDare53/Assets/Scripts/MovementComponent.cs
+++ LudumDare53/Assets/Scripts/MovementComponent.cs
@@ -2,15 +2,21 @@ using UnityEngine;
 
 public class MovementComponent : MonoBehaviour
 {
-    [SerializeField] public float MoveSpeed = 5f;
+    public Rigidbody Rigidbody;
+    public float MoveSpeed = 5f;
 
-	public Vector3 LastMove { get; set; }
+    public Vector3 LastMove { get; set; }
+
+    private void Awake()
+    {
+        Rigidbody = GetComponent<Rigidbody>();
+    }
 
     public void Move(Vector3 direction)
     {
         direction.y = 0f; // Remove any y component
 
-		LastMove = direction.normalized * MoveSpeed * Time.deltaTime;
-        transform.position += LastMove;
+        LastMove = direction.normalized * MoveSpeed * Time.deltaTime;
+        Rigidbody.MovePosition(transform.position + LastMove);
     }
 }
