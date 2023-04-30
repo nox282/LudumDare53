11a26a05da7a26043155db23029f0ed939036977
neakiir <filion.jonathan@gmail.com>
2023-04-30 16:18:08 -0400

    Add basic dialog flow

--- LudumDare53/Assets/Scripts/DialogData.cs
+++ LudumDare53/Assets/Scripts/DialogData.cs
@@ -1,9 +1,44 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "NewDialogData", menuName = "ScriptableObjects/Dialog Data")]
 public class DialogData : ScriptableObject
 {
-	public Texture2D LeftImage;
-	public Texture2D RightImage;
-	public string Text;
+	public enum SpeakerId
+	{
+		Left,
+		Right
+	}
+
+	[System.Serializable]
+	public class Speaker
+	{
+		public Sprite Display;
+		public AudioClip SpeakingSound;
+		public Color Color;
+	}
+
+	[System.Serializable]
+	public class DialogPhase
+	{
+		public SpeakerId SpeakerId;
+
+		public Sprite OverrideDisplayLeft;
+		public Sprite OverrideDisplayRight;
+
+		[TextArea(10, 100)]
+		public string Text;
+
+		public float LetterDelay = 0.1f;
+	}
+
+	public List<DialogPhase> DialogPhases;
+
+	[SerializeField] private Speaker _leftSpeaker;
+	[SerializeField] private Speaker _rightSpeaker;
+
+	public Speaker GetSpeaker(SpeakerId id)
+	{
+		return id == SpeakerId.Left ? _leftSpeaker : _rightSpeaker;
+	}
 }
